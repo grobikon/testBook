27fb2f8b290440a1d5ea7e53eab491cfde6afe81
Образцов Григорий <grobikon@mail.ru>
2021-11-02 21:15:04 +0300

    Create new project test book.

--- src/main/kotlin/com/test/book/repo/BookRepo.kt
+++ src/main/kotlin/com/test/book/repo/BookRepo.kt
@@ -6,16 +6,10 @@ import org.springframework.data.jpa.repository.Query
 
 interface BookRepo: JpaRepository<Book, Long> {
 
-    /**
-     * Поиск всех пользователей, с возможностью пагинации
-     */
     @Query(value = "SELECT * FROM book ORDER BY book_title DESC",
         nativeQuery = true)
     fun getBooksSortTitleDesc(): List<Book>
 
-    /**
-     * Поиск всех пользователей, с возможностью пагинации
-     */
     @Query(value = "SELECT * FROM book ORDER BY book_author",
         nativeQuery = true)
     fun getBooksSortAuthor(): List<Book>
